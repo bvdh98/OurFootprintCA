75e8dce37e333d1b83e42745df01d47fa4733503
ZedTT <47646286+ZedTT@users.noreply.github.com>
2020-05-05 12:38:07 -0700

    working autocomplete with test data

--- frontend/OurFootprint/src/app/components/home/calculator/transportation/transportation.component.ts
+++ frontend/OurFootprint/src/app/components/home/calculator/transportation/transportation.component.ts
@@ -30,24 +30,34 @@ export class TransportationComponent implements OnInit {
     frequency: new FormControl(),
   })
 
-  options: string[] = ['One', 'Two', 'Three']
-  filteredOptions: Observable<string[]>
+  vehicles: Array<{years: number[], name: string}> = [
+    {years: [1985, 1984, 1984], name: 'Alfa Romeo Spider Veloce 2000'},
+    {years: [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1991, 1992], name: 'Ferrari Testarossa'},
+    {years: [1984, 1985, 1986, 1987, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020], name: 'Dodge Charger'},
+    {years: [1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994], name: 'Dodge B150/B250 Wagon 2WD'},
+    {years: [1993, 1994, 1994, 1992, 1992, 1993], name: 'Subaru Legacy AWD Turbo'},
+    {years: [1993, 1993, 1990, 1990, 1990, 1991, 1991, 1992, 1992], name: 'Subaru Loyale'},
+    {years: [1993], name: 'Toyota Corolla'},
+    {years: [1993, 1994, 1995, 1998, 1999, 1999, 2001, 2001, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020], name: 'Volkswagen Golf III / GTI'}, {years: [1993, 1994, 1995, 1996, 1997, 1998, 1999, 2001, 1989, 1989, 1990, 1991, 1991, 1992], name: 'Volkswagen Jetta III'},
+    {years: [1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1995, 1996, 1997, 1998, 1999], name: 'Volvo 240'}]
+  filteredVehicles: Observable<{years: number[], name: string}[]>
 
   constructor() { }
 
   ngOnInit(): void {
     // TODO: load previous commutes from that this user entered (do after init?)
-    this.filteredOptions = this.commuteForm.valueChanges
+    this.filteredVehicles = this.commuteForm.valueChanges
       .pipe(
         startWith(''),
-        map(value => this._filter(value))
+        map(value => this._filter(value.vehicle))
       )
   }
 
-  private _filter(value: string): string[] {
+  private _filter(value: string): {years: number[], name: string}[] {
+    if (value === undefined) { return [] }
     const filterValue = value.toLowerCase()
 
-    return this.options.filter(option => option.toLowerCase().includes(filterValue))
+    return this.vehicles.filter(car => car.name.toLowerCase().includes(filterValue))
   }
 
   addCommute(): void {
