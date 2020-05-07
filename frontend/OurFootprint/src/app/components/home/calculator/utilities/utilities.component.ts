fc03c48ff2ebb0738cac82e3cea48b48846f9702
edmondksleung <edmondksleung@hotmail.com>
2020-05-07 15:23:12 -0700

    Added method to reset error message when user uploads correct file

--- frontend/OurFootprint/src/app/components/home/calculator/utilities/utilities.component.ts
+++ frontend/OurFootprint/src/app/components/home/calculator/utilities/utilities.component.ts
@@ -9,7 +9,7 @@ export class UtilitiesComponent implements OnInit {
 
   private fileFortis: File
   private fileHydro: File
-  private fileExtensionError: string
+  fileExtensionError: string
 
   constructor() { }
 
@@ -17,10 +17,12 @@ export class UtilitiesComponent implements OnInit {
   }
 
   onUploadClickedFortis(fileList) {
+    this.resetFileExtensionError();
     (this.validateFile(fileList[0].name) ? this.fileFortis = fileList[0] : this.fileExtensionError = fileList[0].name + ' is an unsupported file')
   }
 
   onUploadClickedHydro(fileList) {
+    this.resetFileExtensionError();
     (this.validateFile(fileList[0].name) ? this.fileHydro = fileList[0] : this.fileExtensionError = fileList[0].name + ' is an unsupported file extension')
   }
 
