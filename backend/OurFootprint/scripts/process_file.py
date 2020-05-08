75394f09419900d46f1b8dd623f1f84e42e10341
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-08 14:40:48 -0700

    added script to process fortis script

--- backend/OurFootprint/scripts/process_file.py
+++ backend/OurFootprint/scripts/process_file.py
@@ -1,11 +1,27 @@
+import datetime
+
 import pandas as pd
-from calculator.models import FortisBillField
+from calculator.models import FortisBillField, User
 
 
-def process_fortis(file):
+def process_fortis(file, uid):
     data = pd.read_csv(file)
-    consumption = []
+    response = []
 
     for i, row in data.iterrows():
-        consumption.append(row['Billed GJ'])
-        new_entry = FortisBillField()
+        user_entry = User(id=uid, num_people_household=1)
+        user_entry.save()
+        s_date = row['Bill from date']
+        start_date = datetime.datetime.strptime(s_date, " %d/%m/%Y")
+        e_date = row['Bill to date']
+        end_date = datetime.datetime.strptime(s_date, " %d/%m/%Y")
+        num_days = row['# of days']
+        consumption = row['Billed GJ']
+        avg_temp = row['Average temperature']
+        new_entry = FortisBillField(user_id=user_entry, start_date=start_date, end_date=end_date, num_days=num_days,
+                                    consumption=consumption, avg_temp=avg_temp)
+        new_entry.save()
+        response.append({'start_date': s_date, 'end_date': e_date, 'num_days': num_days, 'consumption': consumption,
+                         'avg_temp': avg_temp})
+
+    return response
