f706a9f867a47d2f482d76fb3b9fd14598f90f57
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-07 19:20:50 -0700

    added documentation and minor changes to models

--- backend/OurFootprint/calculator/models.py
+++ backend/OurFootprint/calculator/models.py
@@ -2,16 +2,25 @@ from django.db import models
 
 
 class User(models.Model):
+    """
+    The main user table to map everything else
+    """
     id = models.PositiveIntegerField(primary_key=True)
     num_people_household = models.PositiveSmallIntegerField()
 
 
 class UserCommute(models.Model):
-    commute_id = models.PositiveIntegerField(primary_key=True)
+    """
+    Bridge between user ad commute.
+    """
+    commute_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User.id, on_delete=models.CASCADE)
 
 
 class Commute(models.Model):
+    """
+    Store users' commutes
+    """
     commute_id = models.ForeignKey(UserCommute.commute_id, primary_key=True, on_delete=models.CASCADE)
     car = models.TextField()
     transmission = models.TextField()
@@ -20,6 +29,9 @@ class Commute(models.Model):
 
 
 class FortisBillField(models.Model):
+    """
+    Each entry in this table corresponds to an entry in users' Fortis bill.
+    """
     user_id = models.ForeignKey(User.id, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
@@ -29,6 +41,9 @@ class FortisBillField(models.Model):
 
 
 class HydroBillField(models.Model):
+    """
+    Each entry in this table corresponds to an entry in users' Fortis bill.
+    """
     user_id = models.ForeignKey(User.id, on_delete=models.CASCADE)
     start_date = models.DateField()
     num_days = models.SmallIntegerField()
@@ -36,6 +51,10 @@ class HydroBillField(models.Model):
 
 
 class UserEmissions(models.Model):
+    """
+    Store the users' emissions fo reach month that we have visibility on
+    """
     user_id = models.ForeignKey(User.id, on_delete=models.CASCADE)
-    month = models.SmallIntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
     emission = models.FloatField()
