42a1ca6040265dd5d7c1d02bc5091b57ace00360
Arun333333 <arundeepsingh2910@gmail.com>
2020-05-11 17:28:20 -0700

    minor refactoring and removed magic numbers

--- backend/OurFootprint/scripts/carbon_calculations.py
+++ backend/OurFootprint/scripts/carbon_calculations.py
@@ -4,7 +4,12 @@ from calculator.models import Commute
 
 # These constants are officially provided by fortis bc and bc hydro and are only specific to these companies
 EMISSION_FACTOR_FORTIS = 0.719
-EMISSION_FACTOR_HYDRO = 0.010670
+EMISSION_FACTOR = 0.010670
+MILE_TO_KM_RATIO = 1.609
+GALLON_TO_LITRES_RATIO = 3.785
+PER_100MILES_TO_KM_RATIO = 160.934
+EMISSION_FACTOR_FUEL_PRODUCTION = 0.43
+METRIC_TONNE_TO_KG_RATIO = 1000
 
 
 def hydro_calculations(bill_entries):
@@ -14,7 +19,7 @@ def hydro_calculations(bill_entries):
     :return carbon_footprint: This is the total footprint from the hydro bill
     """
     total_kwh = sum(bill_entries)
-    carbon_footprint = (total_kwh * EMISSION_FACTOR_HYDRO) / 1000  # in tonnes
+    carbon_footprint = (total_kwh * EMISSION_FACTOR) / METRIC_TONNE_TO_KG_RATIO  # in metric tonnes
     return carbon_footprint
 
 
@@ -29,28 +34,28 @@ def fortis_calculations(bill_entries):
     return carbon_footprint
 
 
-class CarbonCalculations:
+class CommuteCalculations:
     def __init__(self, commute: Commute):
         """
-        Initializer for the class , and calls a method  _get_emission_efficiency that checks if the
+        Initializer for the class , method _get_emission_efficiency checks if the
         vehicle is electric or not
         :param commute: commute object stores attributes for each commute.
         """
         self.commute = commute
-        self.city_emissions = 0
-        self.city_fuel_eff = 0
-        self.highway_fuel_eff = 0
-        self.is_electric = False
+        self.common_emissions = None
+        self.city_fuel_eff = None
+        self.highway_fuel_eff = None
+        self.is_electric = None
         self._get_emission_efficiency()
 
     def calculate_footprint(self):
         """
-        This method is called which checks if the vehicle is electric or not and calls the corresponding methods to
+        Checks if the vehicle is electric or not and calls the corresponding methods to
         calculate carbon footprint
         """
 
         distance = self.commute.distance
-        highway_percentage = 100 - self.commute.city_perc
+        highway_percentage = self.commute.highway_perc
         if self.is_electric:
             return self._electric_vehicles_footprint(distance, highway_percentage)
         else:
@@ -74,7 +79,7 @@ class CarbonCalculations:
         highway08: Fuel efficiency of the vehicle on the highway
         :param required_rows: It is a list of dictionary which stores all the required data of a vehicle
         """
-        self.city_emissions = self._mean(required_rows, 'co2TailpipeGpm')
+        self.common_emissions = self._mean(required_rows, 'co2TailpipeGpm')
         self.city_fuel_eff = self._mean(required_rows, 'city08')
         self.highway_fuel_eff = self._mean(required_rows, 'highway08')
 
@@ -99,36 +104,36 @@ class CarbonCalculations:
 
     def _calculate_footprint_gasoline(self, distance, highway_percentage) -> double:
         # converting city  fuel efficiency to km per litres
-        converted_city_fuel_eff = (self.city_fuel_eff * 1.609) / 3.785
+        converted_city_fuel_eff = (self.city_fuel_eff * MILE_TO_KM_RATIO) / GALLON_TO_LITRES_RATIO
 
         # calculating highway distance
         highway_distance = (highway_percentage / 100) * distance
         # calculating highway fuel
-        highway_fuel = highway_distance / ((self.highway_fuel_eff * 1.609) / 3.785)
+        highway_fuel = highway_distance / ((self.highway_fuel_eff * MILE_TO_KM_RATIO) / GALLON_TO_LITRES_RATIO)
         # calculating city distance
         city_distance = distance - highway_distance
         # calculating city fuel
         city_fuel = city_distance / converted_city_fuel_eff
         # converting emissions to KGco2 per km
-        common_emm = self.city_emissions / (1.60934 * 1000)
+        common_emm = self.common_emissions / (MILE_TO_KM_RATIO * METRIC_TONNE_TO_KG_RATIO)
         # calculating footprint for  decomposition city
         fuel_decomposition_city = common_emm * city_distance
         # calculating footprint for  decomposition highway
         fuel_decomposition_highway = highway_distance * common_emm
         # calculating footprint for  fuel production city
-        fuel_production_city = city_fuel * 0.43
+        fuel_production_city = city_fuel * EMISSION_FACTOR_FUEL_PRODUCTION
         # calculating footprint for  fuel production city
-        fuel_production_highway = highway_fuel * 0.43
+        fuel_production_highway = highway_fuel * EMISSION_FACTOR_FUEL_PRODUCTION
         # calculating total footprint for the commute
         total_footprint = (fuel_decomposition_city + fuel_production_city + fuel_decomposition_highway
-                           + fuel_production_highway) / 1000  # in tonnes
+                           + fuel_production_highway) / METRIC_TONNE_TO_KG_RATIO  # in metric tonnes
         print(total_footprint)
         return total_footprint
 
     def _electric_vehicles_footprint(self, distance, highway_percentage):
         # converting the city and highway data into proper units
-        converted_city_kwh = self.city_fuel_eff / 160.934
-        converted_highway_kwh = self.highway_fuel_eff / 160.934
+        converted_city_kwh = self.city_fuel_eff / PER_100MILES_TO_KM_RATIO
+        converted_highway_kwh = self.highway_fuel_eff / PER_100MILES_TO_KM_RATIO
 
         # calculating highway distance
         highway_distance = (highway_percentage / 100) * distance
@@ -139,6 +144,5 @@ class CarbonCalculations:
         # calculating highway kwh
         total_highway_kwh = converted_highway_kwh * highway_distance
         total_kwh = total_city_kwh + total_highway_kwh
-        emission_factor = 0.010670
-        total_footprint = (total_kwh * emission_factor) / 1000  # tonnes
+        total_footprint = (total_kwh * EMISSION_FACTOR) / METRIC_TONNE_TO_KG_RATIO  # metric tonnes
         return total_footprint
