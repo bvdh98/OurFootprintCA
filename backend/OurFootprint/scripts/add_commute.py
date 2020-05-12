f94c46e573aa0607dc7009f037d4dd65d51e6cba
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-12 00:02:35 -0700

    added script to calculate footprint for a user

--- backend/OurFootprint/scripts/add_commute.py
+++ backend/OurFootprint/scripts/add_commute.py
@@ -12,7 +12,7 @@ def add_commute_to_db(commute, uid):
     year = commute.get('year')
     transmission = commute.get('transmission')
     distance = commute.get('distance')
-    city_perc = commute.get('city_perc')
+    highway_perc = commute.get('highway_perc')
 
     # Create a reference to the user-commute bridge
     user_commute_entry = UserCommute(user_id=user_entry)
@@ -20,5 +20,5 @@ def add_commute_to_db(commute, uid):
 
     # Add entry to commute table using the bridge reference
     commute_entry = Commute(commute_id=user_commute_entry, car=vehicle, car_year=year, transmission=transmission,
-                            distance=distance, city_perc=city_perc)
+                            distance=distance, highway_perc=highway_perc)
     commute_entry.save()
--- backend/OurFootprint/scripts/calculate_for_user.py
+++ backend/OurFootprint/scripts/calculate_for_user.py
@@ -1,4 +1,4 @@
-from calculator.models import FortisBillField, HydroBillField, UserCommute, Commute
+from calculator.models import FortisBillField, HydroBillField, UserCommute, Commute, UserEmissions
 
 
 def calculate_footprint_for_user(uid):
@@ -17,6 +17,33 @@ def calculate_footprint_for_user(uid):
 
     user_fortis_footprint = fortis_calculations(fortis_energy)
     user_hydro_footprint = hydro_calculations(hydro_energy)
-    user_transport_footprint = sum([CommuteCalculator(commute).calcuate_footprint() for commute in commute_refs])
+    user_transport_footprint = sum([calculate_commute_footprint(commute) for commute in commute_refs])
 
-    return user_fortis_footprint + user_hydro_footprint + user_transport_footprint
+    total_monthly_footprint = user_fortis_footprint + user_hydro_footprint + user_transport_footprint
+
+    fortis_start_date = get_min_start_date(fortis_ref)
+    hydro_start_date = get_min_start_date(fortis_ref)
+
+    fortis_end_date = get_max_end_date(fortis_ref)
+    hydro_end_date = get_max_end_date(hydro_ref)
+
+    start_date = max(fortis_start_date, hydro_start_date)
+    end_date = min(fortis_end_date, hydro_end_date)
+
+    emission_entry = UserEmissions.objects.get(user_id=uid)
+
+    emission_entry.start_date = start_date
+    emission_entry.end_date = end_date
+    emission_entry.emission = total_monthly_footprint
+
+    emission_entry.save()
+
+
+def get_min_start_date(bills):
+    start_dates = [bill.start_date for bill in bills]
+    return min(start_dates)
+
+
+def get_max_end_date(bills):
+    start_dates = [bill.end_date for bill in bills]
+    return max(start_dates)
