61a3d44850440788fdfe6064ba43f62f23dc077f
Arun333333 <arundeepsingh2910@gmail.com>
2020-05-14 15:15:57 -0700

    changed the route to the dashboard
    Co-authored-by: Edmond Leung <edmondksleung@hotmail.com>
    Co-authored-by: Ben Harrison <bvdhest1998@gmail.com>

--- frontend/OurFootprint/src/app/app-routing.module.ts
+++ frontend/OurFootprint/src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { TestComponent } from './components/test/test.component'
 import { HomeComponent } from './components/home/home.component'
 import { AboutUsComponent } from './components/about-us/about-us.component'
 import { SponsorsComponent } from './components/sponsors/sponsors.component'
-import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { DashBoardComponent } from './components/dash-board/dash-board.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about-us', component: AboutUsComponent},
   {path: 'our-sponsors', component: SponsorsComponent},
   {path: 'test', component: TestComponent},
-  {path: 'dash', component: DashboardComponent},
+  {path: 'dash', component: DashBoardComponent},
 ]
 
 @NgModule({
