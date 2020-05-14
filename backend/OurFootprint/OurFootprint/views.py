b67b5166e06712fa229960f4e1ae6ff7e756faab
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-14 01:21:15 -0700

    added GET and DELETE endpoints

--- backend/OurFootprint/OurFootprint/views.py
+++ backend/OurFootprint/OurFootprint/views.py
@@ -7,6 +7,8 @@ from django.http import JsonResponse
 from django.utils.datastructures import MultiValueDictKeyError
 from django.views.decorators.csrf import csrf_exempt
 
+from scripts.dels import del_fortis, del_hydro, del_commute
+from scripts.gets import get_fortis, get_hydro, get_commute, get_fortis_with_id, get_hydro_with_id, get_commute_with_id
 from scripts.process_file import process_fortis, process_hydro
 from scripts.add_commute import add_commute_to_db
 from scripts.calculate_for_user import calculate_footprint_for_user
@@ -24,9 +26,10 @@ def e2(request):
 
 @csrf_exempt
 @login_required
-def fortis_bill(request):
+def fortis_bill(request, pk=0):
     response = []
     status = 500
+    uid = request.user.id
     if request.method == 'POST':
         try:
             file = request.FILES['fortis']
@@ -34,14 +37,22 @@ def fortis_bill(request):
             response = {'error': 'Unexpected key, expected "fortis"'}
             status = 400
         else:
-            response = process_fortis(file, 100)
+            response = process_fortis(file, uid)
             status = 200
+    elif request.method == 'GET':
+        if pk == 0:
+            response = get_fortis(uid)
+        else:
+            response = get_fortis_with_id(pk)
+    elif request.method == 'DELETE':
+        del_fortis(pk)
     return JsonResponse(response, safe=False, status=status)
 
 
 @csrf_exempt
 @login_required
-def hydro_bill(request):
+def hydro_bill(request, pk=0):
+    uid = request.user.id
     response = []
     status = 500
     if request.method == 'POST':
@@ -51,18 +62,33 @@ def hydro_bill(request):
             response = {'error': 'Unexpected key, expected "hydro"'}
             status = 400
         else:
-            response = process_hydro(file, 100)
+            response = process_hydro(file, uid)
             status = 200
+    elif request.method == 'GET':
+        if pk == 0:
+            response = get_hydro(uid)
+        else:
+            response = get_hydro_with_id(pk)
+    elif request.method == 'DELETE':
+        del_hydro(pk)
     return JsonResponse(response, safe=False, status=status)
 
 
 @csrf_exempt
 @login_required(login_url='/api/signup')
-def add_commute(request):
+def add_commute(request, pk=0):
+    uid = request.user.id
     response = []
     if request.method == 'POST':
         commute = json.loads(request.body)
-        response = add_commute_to_db(commute, 100)
+        response = add_commute_to_db(commute, uid)
+    elif request.method == 'GET':
+        if pk == 0:
+            response = get_commute(uid)
+        else:
+            response = get_commute_with_id(uid)
+    elif request.method == 'DELETE':
+        del_commute(pk)
     return JsonResponse(response, safe=False)
 
 
@@ -74,7 +100,8 @@ def get_vehicles_json(request):
 
 
 def calculate_footprint(request):
-    response = calculate_footprint_for_user(100)
+    uid = request.user.id
+    response = calculate_footprint_for_user(uid)
     return JsonResponse(response)
 
 
