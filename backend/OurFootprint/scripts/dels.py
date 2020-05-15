2399a1e02839f62a39984b443dfc58997292b6d2
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-15 02:03:26 -0700

    made changes

--- backend/OurFootprint/scripts/dels.py
+++ backend/OurFootprint/scripts/dels.py
@@ -5,7 +5,7 @@ def del_fortis(uid, fortis_id):
     instance = FortisBillField.objects.get(id=fortis_id)
     if instance is None:
         return {"error": "Not found"}, 404
-    if instance.user_id != uid:
+    if instance.user_id.id != uid:
         return {"error": "Access denied"}, 403
     instance.delete()
     return {}, 200
@@ -15,7 +15,7 @@ def del_hydro(uid, hydro_id):
     instance = HydroBillField.objects.get(id=hydro_id, user_id=uid)
     if instance is None:
         return {"error": "not found"}, 404
-    if instance.user_id != uid:
+    if instance.user_id.id != uid:
         return {"error": "Access denied"}, 403
     instance.delete()
     return {}, 200
