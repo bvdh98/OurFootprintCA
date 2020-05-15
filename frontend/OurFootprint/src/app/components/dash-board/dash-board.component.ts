5c12e72c879477918c7fbb2aff872af7e518b9f8
Arun333333 <arundeepsingh2910@gmail.com>
2020-05-14 18:13:53 -0700

    changed functionality to autoload labels
    Co-authored-by: Ben Harrison <bvdhest1998@gmail.com>
    Co-authored-by: Edmond Leung <edmondksleung@hotmail.com>
    Co-authored-by: dhruvparekh01 <dhruvparekh01@gmail.com>

--- frontend/OurFootprint/src/app/components/dash-board/dash-board.component.ts
+++ frontend/OurFootprint/src/app/components/dash-board/dash-board.component.ts
@@ -17,7 +17,7 @@ export class DashBoardComponent implements OnInit {
   totalChartPlugins = []
   totalChartData: ChartDataSets[]
 
-  fortisChartLabels: Label[] = ['jan', 'feb', 'march', 'april', 'may', 'June', 'July']
+  fortisChartLabels: Label[]
   fortisChartData: ChartDataSets[]
   fortisChartType: ChartType = 'bar'
   fortisChartPlugins = []
@@ -29,7 +29,7 @@ export class DashBoardComponent implements OnInit {
   hydroChartOptions: ChartOptions = {
     responsive: true,
   }
-  hydroChartLabels: Label[] = ['jan', 'feb', 'march']
+  hydroChartLabels: Label[] 
   hydroChartType: ChartType = 'bar'
   hydroChartLegend = true
   hydroChartPlugins = []
@@ -43,21 +43,27 @@ export class DashBoardComponent implements OnInit {
   vehicleChartLegend = true
   vehicleChartPlugins = []
   vehicleChartData: ChartDataSets[]
-  readonly data: string = 'to do'
-  constructor() { }
+  totalFootprint: number
+  commuteFootprint: number
+  fortisFootprint: number
+  hydroFootprint: number
+  totalTrees: number
+  dollars: number
+
+
 
   // TODO: Remove hardcoded data
   readonly userdata = {
-    fortis: [{ start_date: '2020-03-24', end_date: '2020-04-21', num_days: 29, consumption: 5.4, footprint: 0.0038826 },
-    { start_date: '2020-02-22', end_date: '2020-03-23', num_days: 31, consumption: 8.6, footprint: 0.0061833999999999995 },
-    { start_date: '2020-01-24', end_date: '2020-02-21', num_days: 29, consumption: 10.8, footprint: 0.0077652 },
-    { start_date: '2019-12-24', end_date: '2020-01-23', num_days: 31, consumption: 11.6, footprint: 0.0083404 },
-    { start_date: '2019-11-22', end_date: '2019-12-23', num_days: 32, consumption: 13.4, footprint: 0.0096346 },
-    { start_date: '2019-10-24', end_date: '2019-11-21', num_days: 29, consumption: 11.3, footprint: 0.0081247 },
-    { start_date: '2019-09-20', end_date: '2019-10-23', num_days: 34, consumption: 10.9, footprint: 0.0078371 }],
-    hydro: [{ start_date: '2020-01-01', num_days: 31, consumption: 936.0, footprint: 0.00998712 },
-    { start_date: '2020-02-01', num_days: 29, consumption: 880.0, footprint: 0.0093896 },
-    { start_date: '2020-03-01', num_days: 31, consumption: 1136.0, footprint: 0.01212112 }],
+    fortis: [{ start_date: '2020-03-24', end_date: '2020-04-21', num_days: 29, consumption: 5.4, footprint: 0.0038826 , month: 'April' },
+    { start_date: '2020-02-22', end_date: '2020-03-23', num_days: 31, consumption: 8.6, footprint: 0.0061833999999999995 , month: 'march' },
+    { start_date: '2020-01-24', end_date: '2020-02-21', num_days: 29, consumption: 10.8, footprint: 0.0077652 , month: 'February' },
+    { start_date: '2019-12-24', end_date: '2020-01-23', num_days: 31, consumption: 11.6, footprint: 0.0083404 , month: 'January' },
+    { start_date: '2019-11-22', end_date: '2019-12-23', num_days: 32, consumption: 13.4, footprint: 0.0096346 , month: 'December' },
+    { start_date: '2019-10-24', end_date: '2019-11-21', num_days: 29, consumption: 11.3, footprint: 0.0081247 , month: 'November' },
+    { start_date: '2019-09-20', end_date: '2019-10-23', num_days: 34, consumption: 10.9, footprint: 0.0078371 , month: 'October' }],
+    hydro: [{ start_date: '2020-01-01', num_days: 31, consumption: 936.0, footprint: 0.00998712 , month: 'January' },
+    { start_date: '2020-02-01', num_days: 29, consumption: 880.0, footprint: 0.0093896 , month: 'February' },
+    { start_date: '2020-03-01', num_days: 31, consumption: 1136.0, footprint: 0.01212112 , month: 'March' }],
     commute: [{
       vehicle: 'Alfa Romeo Spider Veloce 2000', vehicle_year: 1985, transmission: 'Manual 5-spd',
       distance: 100.0, footprint: 0.0,
@@ -65,6 +71,7 @@ export class DashBoardComponent implements OnInit {
     { vehicle: 'Alfa Romeo Spider Veloce 2000', vehicle_year: 1985, transmission: 'Manual 5-spd', distance: 100.0, footprint: 0.0 },
     { vehicle: 'Alfa Romeo Spider Veloce 2000', vehicle_year: 1985, transmission: 'Manual 5-spd', distance: 100.0, footprint: 0.0 }],
   }
+  constructor() { }
 
   ngOnInit(): void {
     this.totalChartData = [
@@ -75,6 +82,9 @@ export class DashBoardComponent implements OnInit {
       { data: this.fortis_values(), label: 'Monthly Footprint in metric tonnes' },
     ]
 
+    this.fortisChartLabels = this.fortis_labels()
+    this.hydroChartLabels = this.hydro_labels()
+
     this.hydroChartData = [
       { data: this.hydro_values(), label: 'Monthly Footprint in metric tonnes' },
 
@@ -82,27 +92,35 @@ export class DashBoardComponent implements OnInit {
     this.vehicleChartData = [
       { data: [this.total_footprint_commute() , 0.10] , label: 'Commute Footprint'},
     ]
+    this.totalFootprint = this.total_footprint_user()
+    this.commuteFootprint = this.total_footprint_commute()
+    this.fortisFootprint = this.total_footprint_fortis()
+    this.hydroFootprint = this.total_footprint_hydro()
+    this.totalTrees = this.footprint_to_tree()
+    this.dollars = this.tree_to_dollars()
     console.log('Dollars' , this.tree_to_dollars())
     console.log('Trees' , this.footprint_to_tree())
+    console.log(this.totalFootprint)
   }
 
   total_footprint_fortis() {
     const totalFoot = this.userdata.fortis.reduce((sum, current) => sum + current.footprint, 0)
     console.log('This is fortis total footprint', totalFoot)
-    return totalFoot
+    return Math.round(totalFoot * 100) / 100
   }
 
   total_footprint_hydro() {
+    console.log(this.userdata)
     const totalFoot = this.userdata.hydro.reduce((sum, current) => sum + current.footprint, 0)
     console.log('This is hydro total footprint', totalFoot)
-    return totalFoot
+    return Math.round(totalFoot * 100) / 100
   }
 
   total_footprint_commute() {
 
     const totalFoot = this.userdata.commute.reduce((sum, current) => sum + current.footprint, 0)
     console.log('This is total commute footprint', totalFoot)
-    return totalFoot
+    return Math.round(totalFoot * 100) / 100
   }
 
   total_footprint_user() {
@@ -111,7 +129,7 @@ export class DashBoardComponent implements OnInit {
     const commuteFootprint: number = this.total_footprint_commute()
     const totalFoot = hydroFootprint + fortisFootprint + commuteFootprint
     console.log('Total user footprint' + totalFoot)
-    return totalFoot
+    return Math.round(totalFoot * 100) / 100
   }
 
   fortis_values() {
@@ -144,6 +162,20 @@ export class DashBoardComponent implements OnInit {
     const noOfTrees = this.footprint_to_tree()
     const costOfOneTree = 4
     return noOfTrees * costOfOneTree
+  }
+  fortis_labels(){
+    const allValues = []
+    for (const dataEntry of this.userdata.fortis){
+      allValues.push(dataEntry.month)
+    }
+    return allValues
+  }
+  hydro_labels(){
+    const allValues = []
+    for (const dataEntry of this.userdata.hydro){
+      allValues.push(dataEntry.month)
+    }
+    return allValues
   }
 
 
