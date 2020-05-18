2ce155cdc53052c28542e57d2946cd7ececabcec
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-18 16:13:03 -0700

    removed users app

--- backend/OurFootprint/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import SampleUser
-
-
-# Serializer to handle conversions between JSON and Python objects and vice versa
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SampleUser
-        fields = ('id', 'name', 'footprint')
