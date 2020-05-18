c980a36de1f977587742544fc42e3f90afe74577
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-18 16:12:01 -0700

    created commute app

---
+++ backend/OurFootprint/commute/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from commute.models import Commute
+
+
+class CommuteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commute
+        fields = ('commute_id', 'vehicle', 'year', 'transmission', 'distance')
