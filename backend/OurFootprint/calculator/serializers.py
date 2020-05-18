f2357e587d995e9827f47d6368fedeb3b7f5f8b0
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-05-18 16:10:47 -0700

    removed calculator app

--- backend/OurFootprint/calculator/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from calculator.models import Commute
-
-
-class CommuteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Commute
-        fields = ('commute_id', 'vehicle', 'vehicle_year', 'transmission', 'distance')
