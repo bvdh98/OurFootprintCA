ef1784e34e447d8c206ae976b5035852af73ac9e
Arun333333 <arundeepsingh2910@gmail.com>
2020-05-19 15:42:25 -0700

    added password must be a  match logic

--- frontend/OurFootprint/src/app/components/signup/signup.component.ts
+++ frontend/OurFootprint/src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { LoginService } from 'src/app/services/login.service'
 
 @Component({
   selector: 'app-signup',
@@ -10,20 +11,48 @@ export class SignupComponent implements OnInit {
   form = new FormGroup({
     username: new FormControl(),
     password: new FormControl(),
+    confirmpassword: new FormControl(),
     email: new FormControl(),
   })
 
-  constructor() { }
+  constructor(private loginService: LoginService , private formBuilder: FormBuilder) { }
+errormessage = ''
+disabledBtn: boolean
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirmpassword: '',
+    })
 
-  // ngOnInit(): void {
-  // }
+    this.disabledBtn = true
 
-  // signUp() {
-  //   const formValues = this.form.value
-  //   this.loginService.signUp({
-  //     username: formValues.username,
-  //     password: formValues.password,
-  //   }).toPromise()
-  // }
+    this.onChanges()
 
+  }
+  onChanges(): void {
+    this.form.get('confirmpassword').valueChanges.subscribe(val => {
+      if (val === '') {
+        this.disabledBtn = true
+      }
+      else if (val !== this.form.value.password) {
+        this.errormessage = 'Passwords do not match'
+        this.disabledBtn = true
+      }
+      else {
+        this.errormessage = ''
+        this.disabledBtn = false
+      }
+    })
+  }
+  signUp() {
+    const formValues = this.form.value
+    this.loginService.signUp({
+      username: formValues.username,
+      password: formValues.password,
+      email: formValues.Email,
+    }).toPromise()
+
+  }
 }
