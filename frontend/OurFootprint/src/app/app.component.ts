28d52b93dc8e57423c9146d6d6eff3f3cc4d7a2b
Arun333333 <arundeepsingh2910@gmail.com>
2020-05-21 14:18:10 -0700

    added login and log out in navbar
    Co-authored-by: dhruvparekh01 <dhruvparekh01@gmail.com>
    Co-authored-by: Zack Taylor <zackthomastaylor+github@gmail.com>

--- frontend/OurFootprint/src/app/app.component.ts
+++ frontend/OurFootprint/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core'
+import { Component , OnInit } from '@angular/core'
+import { LoginService } from './services/login.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'OurFootprint'
+
+  isLoggedIn = false
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit(): void {
+    this.loginService.currentLoggedInStatus.subscribe(status => this.isLoggedIn = status)
+    this.loginService.isLogged().subscribe((response) => {
+      this.isLoggedIn = response.is_logged_in
+    })
+  }
+
+  logout(): void {
+    this.loginService.logOut().subscribe(() => {
+      this.loginService.changeLoggedInStatus(false)
+      this.isLoggedIn = false
+    })
+  }
 }
